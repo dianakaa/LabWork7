60e091aa5ce65b65b9b6d12281ac76b4c2257b79
ki1lmyself <sofiamiklzkova@gmail.com>
2024-06-22 17:39:43 +0300

    Добавьте файлы проекта.

---
+++ LabWork7/Book.cs
@@ -0,0 +1,40 @@
+﻿namespace LabWork7
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+
+        public Book() : this("Собачье cердце", "М.А. Булгаков", 888) { }
+
+        public Book(string name, string author, int price)
+        {
+            Name = name;
+            Author = author;
+            Price = price;
+        }
+
+        public static Book operator ++(Book book)
+            => new() { Name = book.Name, Author = book.Author, Price = book.Price + 1 };
+
+        public static Book operator +(Book book1, Book book2)
+            => new() { Price = book1.Price + book2.Price, Name = book1.Name, Author = book1.Author };
+
+        public static bool operator ==(Book book1, Book book2) 
+            => book1.Price == book2.Price && book1.Name == book2.Name && book1.Author == book2.Author;
+
+        public static bool operator !=(Book book1, Book book2) 
+            => book1.Price != book2.Price || book1.Name != book2.Name || book1.Author != book2.Author;
+
+        public static bool operator true(Book book4) => book4.Price >= 0;
+
+        public static bool operator false(Book book4) => book4.Price < 0;
+
+        public void Print()
+        {
+            Console.WriteLine($"Название: {Name}, Автор: {Author}, Цена: {Price}");
+        }
+    }
+
+}
---
+++ LabWork7/Program.cs
@@ -0,0 +1,38 @@
+﻿using LabWork7;
+
+Console.WriteLine("Исходные значения:\n");
+
+Book book = new();
+book.Print();
+
+Book book1 = new("Евгений Онегин", "A.C.Пушкин", 340);
+book1.Print();
+
+Book book2 = new("Мастер и Маргарита", "М.А. Булгаков", 488);
+book2.Print();
+
+book++;
+Console.WriteLine("Значения после ++: ");
+book.Print();
+
+Book book3 = new Book();
+book3.Price = book1.Price + book2.Price;
+Console.WriteLine("Значения после +: ");
+book3.Print();
+
+if (book1 == book2)
+    Console.WriteLine("Книги равны");
+else
+    Console.WriteLine("Книги не равны");
+
+if (book1 != book2)
+    Console.WriteLine("Книги не равны");
+else
+    Console.WriteLine("Книги равны");
+
+Book book4 = new() { Price = -138 };
+
+if (book4)
+    Console.WriteLine("Цена больше или равна 0");
+else
+    Console.WriteLine("Цена меньше 0");
